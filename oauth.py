ceb4cf06e1470445008be83cd788a8b809398df0
iChemy <controlyuu.dev@gmail.com>
2023-12-14 23:11:24 +0900

    add comment

--- oauth.py
+++ oauth.py
@@ -1,14 +1,13 @@
 from uuid import uuid4
 from fastapi import APIRouter, Response, Depends, HTTPException
 from sessionstore import cookie, backend, session_verifier ,SessionData
-import hashlib
-import base64
 import secrets
 import os
 import traq
 from traq.api import oauth2_api
 from traq.model.o_auth2_token import OAuth2Token
 from pprint import pprint
+from pydantic import BaseModel
 
 router = APIRouter()
 
@@ -20,17 +19,26 @@ client_id = os.getenv("CLIENT_ID", "jD70ycOCGcUfdTSmU6ORuAjVmSUYsJ4kyNxG")
 async def get_oauth_redirect(response: Response):
 
     # セッションIDを生成
-    session = uuid4()
+    session_id = uuid4()
     verifier = secrets.token_hex(32)
     data = SessionData(verifier=verifier, accessToken=None)
 
 
-    await backend.create(session, data)
-    cookie.attach_to_response(response, session)
+    await backend.create(session_id, data)
+    cookie.attach_to_response(response, session_id)
     uri = f"{traq_base_path}/oauth2/authorize?response_type=code&client_id={client_id}"
 
     return {"redirect_uri": uri}
 
+
+# post_o_auth2_token のレスポンスの型
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
+    expires_in: int
+    refresh_token: str
+    scope: str
+    id_token: str
 
 # The client must configure the authentication and authorization parameters
 # in accordance with the API server security policy.
@@ -43,27 +51,37 @@ configuration = traq.Configuration(
 )
 
 @router.get("/callback", dependencies=[Depends(cookie)])
-async def get_oauth_callback(code: str, session_data: SessionData = Depends(session_verifier)):
+async def get_oauth_callback(code: str,response: Response , session_id: uuid4 = Depends(session_verifier)):
     if code == "":
         raise HTTPException(status_code=400, detail="code is empty")
-    api_response = None
-    # Enter a context with an instance of the API client
+    api_response: TokenResponse = None
+
     with traq.ApiClient(configuration) as api_client:
         # Create an instance of the API class
         api_instance = oauth2_api.Oauth2Api(api_client)
-        grant_type = "authorization_code" # str | 
-        code = code # str |  (optional)
+        # "authorization_code" で固定
+        grant_type = "authorization_code"
+        # code というクエリパラメータから得られる値 (必須)
+        code = code
 
         # example passing only required values which don't have defaults set
         # and optional values
         try:
             # OAuth2 トークンエンドポイント
             api_response = api_instance.post_o_auth2_token(grant_type, code=code, client_id=client_id)
-            pprint(api_response)
+            if len(api_response.access_token) == 0:
+                raise HTTPException(status_code=400, detail="Bad Request\n")
         except traq.ApiException as e:
-            print("Exception when calling Oauth2Api->post_o_auth2_token: %s\n" % e)
+            raise HTTPException(status_code=500, detail=f"Exception when calling Oauth2Api->post_o_auth2_token: {e}\n")
+        
+        # 一応バックエンドにアクセストークンを保存
+        session_data: SessionData = backend.read(session_id)
+        session_data.accessToken = api_response.access_token
+        await backend.update(session_id, session_data)
 
-    return {"responce": str(api_response)}
+        # クッキーにセッションIDを貼り付ける
+        cookie.attach_to_response(response, session_id)
+    return {"access_token": api_response.access_token}
 
 @router.get("/access-token", dependencies=[Depends(cookie)])
 async def get_access_token(session_data: SessionData = Depends(session_verifier)):
